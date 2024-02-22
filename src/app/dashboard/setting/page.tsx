bc40215f87ee173b53622a7e0278b3b8c00d6bf1
cloud <yuban0521@163.com>
2024-02-22 17:10:58 +0800

    ant design

---
+++ src/app/dashboard/setting/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <div>DashboardSetting Page</div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div className="home">
+      {/*<Link href="/login">Sign in</Link>*/}
+      <Button type={"primary"}>||||</Button>
+    </div>
+  );
+}
